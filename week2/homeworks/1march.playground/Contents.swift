1449937f4248d2787230b4d26775a944e3ab347e
lale hüseyin <lalehuseyn@marun.edu.tr>
2024-03-01 00:21:18 +0300

    questions

---
+++ week2/homeworks/1march.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+/*Fonksiyona parametre olarak verilen sayıya göre + - karakterlerini ekrana yazdıran bir fonksiyon yaınız.
+ Örneğin 1 için sadece +, 2 için +-, 5 için +-+-+ şeklinde olmalıdır*/
+
+
+
+
+
+
+/*Fonksiyona parametre olarak verilen sayıyı en büyük yapacak şekilde 5 sayısını yanına koyunuz.
+ Örneğin parametre 0 için çıktı 50 olmalıdır. Parametre 528 için 285, parametre 920 için 9520 olmalıdır*/
